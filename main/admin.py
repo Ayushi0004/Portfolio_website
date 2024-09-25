2d3b5fd930b3d0ccce9dcb45663b1bd20322b3a5
ayushi0004 <ayushichaudhari404@gmail.com>
2024-09-25 23:30:12 +0530

    models

--- main/admin.py
+++ main/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from .models import HomeContent, About, Project, Certificate, Contact
+
+admin.site.register(HomeContent)
+admin.site.register(About)
+admin.site.register(Project)
+admin.site.register(Certificate)
+admin.site.register(Contact)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+class HomeContent(models.Model):
+    title = models.CharField(max_length=200, default="Welcome to My Portfolio")
+    description = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
+
+class About(models.Model):
+    title = models.CharField(max_length=100, default="About Me")
+    description = models.TextField()
+    profile_image = models.ImageField(upload_to='about/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/', blank=True, null=True)
+    link = models.URLField(blank=True, null=True)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+class Certificate(models.Model):
+    title = models.CharField(max_length=200)
+    issuing_organization = models.CharField(max_length=200)
+    issue_date = models.DateField()
+    certificate_file = models.FileField(upload_to='certificates/', blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.title} from {self.issuing_organization}"
+
+class Contact(models.Model):
+    email = models.EmailField()
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    linkedin = models.URLField(blank=True, null=True)
+    github = models.URLField(blank=True, null=True)
+    twitter = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.email
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    link = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
